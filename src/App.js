384f8a7e1796eeea3a3d10953075148900c1b64f
Justin-Sherfey <jsherfey505@gmail.com>
2022-03-16 01:40:55 -0700

    ability to add home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
+import Home from './pages/home/Home';
 import "./app.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Topbar/>
       <div className="container">
         <Sidebar/>
+        <Home/>
       </div>
     </div>
   );
